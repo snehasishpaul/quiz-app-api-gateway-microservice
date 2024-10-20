bc4202068b3db9d8add61dcc5f517a8736130450
Snehasish Paul <weishenpov@gmail.com>
2024-10-20 16:41:06 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/snehasish/microservice/api_gateway/QuizAppApiGatewayMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppApiGatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppApiGatewayMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snehasish/microservice/api_gateway/QuizAppApiGatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApiGatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
